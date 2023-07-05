4e4f7db0fe0eeb1db22ccd1e6cb1e8edb7c3afd3
Kimbangul <highcolor_12@naver.com>
2023-07-05 10:59:14 +0900

    feat: bind score state

--- src/components/score/Score.tsx
+++ src/components/score/Score.tsx
@@ -1,23 +1,34 @@
 import { ScoreStyle as SS } from "./ScoreStyle";
+import { useRecoilValue } from "recoil";
+import { gameState, scoreState, BLACK, WHITE } from "../../recoil/stage";
+import { GameStateType, ScoreStateType } from "../../recoil/stage";
 
 const Score = () => {
+  const game = useRecoilValue<GameStateType>(gameState);
+  const score = useRecoilValue<ScoreStateType>(scoreState);
+
   return(
-    <SS.Container>
-      <SS.Title>SCORE</SS.Title>
-      <SS.Player.Container>
-        <SS.Player.Item>
-          <span>BLACK</span>
-          <SS.Player.Score>0</SS.Player.Score>
-        </SS.Player.Item>
-        <SS.Player.Item>
-          :
-        </SS.Player.Item>
-        <SS.Player.Item>
-          <span>WHITE</span>
-          <SS.Player.Score>0</SS.Player.Score>
-        </SS.Player.Item>
-      </SS.Player.Container>
-    </SS.Container>
+    <>
+      {
+        game === 'start' &&
+        <SS.Container>
+          <SS.Title>SCORE</SS.Title>
+          <SS.Player.Container>
+            <SS.Player.Item>
+              <span>BLACK</span>
+              <SS.Player.Score>{score[BLACK]}</SS.Player.Score>
+            </SS.Player.Item>
+            <SS.Player.Item>
+              :
+            </SS.Player.Item>
+            <SS.Player.Item>
+              <span>WHITE</span>
+              <SS.Player.Score>{score[WHITE]}</SS.Player.Score>
+            </SS.Player.Item>
+          </SS.Player.Container>
+        </SS.Container>
+      }
+    </>
   )
 }
 
