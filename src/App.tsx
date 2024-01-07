87daf4b3bed1905363c3b19a9d805fbc9cea95a5
Kimbangul <highcolor_12@naver.com>
2024-01-08 08:48:46 +0900

    chore: install axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import InputContainer from "./components/info/InputContainer";
 import { AppStyle as A } from "./style/AppStyle";
 import ResetButton from "./components/reset/ResetButton";
 import Score from "./components/score/Score";
+import socket from 'socket.io';
 
 function App() {
   return (
