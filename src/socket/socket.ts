dfff31109e4470bb7939e9911f4b2da272721c3b
Kimbangul <highcolor_12@naver.com>
2024-01-15 08:25:39 +0900

    feat

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,5 +1,25 @@
 import {io} from 'socket.io-client';
 
+// Class Socket(){
+//   constructor(){
+//     this.socket;
+//   }
+
+//   initClientSocket(){
+//     this.socket = io("http://127.0.0.1:8000", {
+//       reconnectionDelayMax: 10000,
+//       withCredentials: true,
+//     });
+  
+//     socket.on("connect", () => {
+//       console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+//     });
+//     socket.on("disconnect", () => {
+//       console.log(socket.id); // undefined
+//     });
+//   }
+// }
+
 export default function initClientSocket(){
   const socket = io("http://127.0.0.1:8000", {
     reconnectionDelayMax: 10000,
@@ -13,3 +33,11 @@ export default function initClientSocket(){
     console.log(socket.id); // undefined
   });
 }
+
+   
+// $("#msgForm").on('submit', (e)=>{
+//   // 소켓메세지 전송
+//   let msg = $('#msg').val();
+//   socket.emit('newMsg', msg)
+//   e.preventDefault();
+// });
